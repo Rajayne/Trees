e57b59ff84b4ce597676844e1cddd96a8067e4c6
Rajayne <ryann.j.nealey@gmail.com>
2023-05-31 15:24:09 -0700

    Create recursive sumValues function and pass test

--- tree.js
+++ tree.js
@@ -15,21 +15,30 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    if (!this.root) return 0;
+
+    let sum = this.root.val;
+
+    function sumHelper(node) {
+      for (let child of node.children) {
+        sum += child.val;
+        if (child.children.length > 0) {
+          sumHelper(child);
+        }
+      }
+    }
+    sumHelper(this.root);
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
-  countEvens() {
-
-  }
+  countEvens() {}
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
-  numGreater(lowerBound) {
-
-  }
+  numGreater(lowerBound) {}
 }
 
 module.exports = { Tree, TreeNode };
