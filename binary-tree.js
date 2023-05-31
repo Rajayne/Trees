b0f1027cc2958908b20f6fb326d5aff1976c4214
Rajayne <ryann.j.nealey@gmail.com>
2023-05-31 15:39:02 -0700

    Create areCousins function and pass test

--- binary-tree.js
+++ binary-tree.js
@@ -97,7 +97,38 @@ class BinaryTree {
    * areCousins(node1, node2): determine whether two nodes are cousins
    * (i.e. are at the same level but have different parents. ) */
 
-  areCousins(node1, node2) {}
+  areCousins(node1, node2) {
+    if (node1 === this.root || node2 === this.root) return false;
+
+    function findLevelAndParent(
+      nodeToFind,
+      currentNode,
+      level = 0,
+      data = { level: 0, parent: null }
+    ) {
+      if (data.parent) return data;
+      if (currentNode.left === nodeToFind || currentNode.right === nodeToFind) {
+        data.level = level + 1;
+        data.parent = currentNode;
+      }
+      if (currentNode.left) {
+        findLevelAndParent(nodeToFind, currentNode.left, level + 1, data);
+      }
+      if (currentNode.right) {
+        findLevelAndParent(nodeToFind, currentNode.right, level + 1, data);
+      }
+      return data;
+    }
+
+    let node1Info = findLevelAndParent(node1, this.root);
+    let node2Info = findLevelAndParent(node2, this.root);
+
+    let sameLevel =
+      node1Info && node2Info && node1Info.level === node2Info.level;
+    let differentParents =
+      node1Info && node2Info && node1Info.parent !== node2Info.parent;
+    return sameLevel && differentParents;
+  }
 
   /** Further study!
    * serialize(tree): serialize the BinaryTree object tree into a string. */
