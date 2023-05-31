21ae03a20298e80e296607bc19d6f27bffe7add9
Rajayne <ryann.j.nealey@gmail.com>
2023-05-31 15:36:31 -0700

    Create maxDepth function and pass test

--- binary-tree.js
+++ binary-tree.js
@@ -34,7 +34,19 @@ class BinaryTree {
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
-  maxDepth() {}
+  maxDepth() {
+    if (!this.root) return 0;
+
+    function maxDepthHelper(node) {
+      if (node.left === null && node.right === null) return 1;
+      if (node.left === null) return maxDepthHelper(node.right) + 1;
+      if (node.right === null) return maxDepthHelper(node.left) + 1;
+      return (
+        Math.max(maxDepthHelper(node.left), maxDepthHelper(node.right)) + 1
+      );
+    }
+    return maxDepthHelper(this.root);
+  }
 
   /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
