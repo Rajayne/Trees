f1cda555ef96b52f1fe3e02143efec9c6f72ba9c
Rajayne <ryann.j.nealey@gmail.com>
2023-05-31 15:35:05 -0700

    Create minDepth function and pass test

--- binary-tree.js
+++ binary-tree.js
@@ -16,7 +16,20 @@ class BinaryTree {
   /** minDepth(): return the minimum depth of the tree -- that is,
    * the length of the shortest path from the root to a leaf. */
 
-  minDepth() {}
+  minDepth() {
+    if (!this.root) return 0;
+
+    function minDepthHelper(node) {
+      if (node.left === null && node.right === null) return 1;
+      if (node.left === null) return minDepthHelper(node.right) + 1;
+      if (node.right === null) return minDepthHelper(node.left) + 1;
+      return (
+        Math.min(minDepthHelper(node.left), minDepthHelper(node.right)) + 1
+      );
+    }
+
+    return minDepthHelper(this.root);
+  }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
